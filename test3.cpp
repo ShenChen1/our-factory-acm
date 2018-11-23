1adf60fa47260a0fff9275650279ea8996675e33
sc <peterchenshen@gmail.com>
2018-11-23 12:15:06 +0800

    Update test3.cpp

--- test3.cpp
+++ test3.cpp
@@ -1,174 +1,187 @@
-﻿#pragma GCC push_options
+#pragma GCC push_options
 #pragma GCC optimize ("-O3")
 #include <fstream>
+#include <iostream>
 #include <cstring>
 #include <string>
+#include <stdarg.h>
 #include <stdlib.h>
 
-#define infof(fmt, arg...) //printf(fmt, ##arg)
-#define debugf(fmt, arg...) //printf(fmt, ##arg)
-#define errorf(fmt, arg...) //printf(fmt, ##arg)
+#define MAXUSER    (10)
+#define MAXLINE    (20)
 
-#define NoEdge 		(-1)
-#define MAX_USER	(10)
-#define MAX_LINE	(20)
+typedef enum
+{
+    ZERO = 0,
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+} number_e;
 
 typedef struct
 {
-	int num;
-	int list[MAX_LINE];
+    int num;
+    int list[MAXLINE];
 } user_t;
 
 typedef struct
 {
-	int user_num;
-	user_t usrs[MAX_USER];
+    int user_num;
+    user_t usrs[MAXUSER];
 
-	int line_num;
-	int lines[MAX_LINE];
+    int line_num;
+    int lines[MAXLINE];
 
 } testline_t;
 
 static testline_t s_testline = {0};
 
-static int get_users_and_lines(char *str)
+static void myprintf(int level, const char *fmt, ...)
 {
-	int i = 0;
-	int ret = 0;
-
-	ret = sscanf(str, "%d, %d", &s_testline.line_num, &s_testline.user_num);
-	if (ret < 0)
-	{
-		errorf("sscanf err\n");
-		return -1;
-	}
-	
-	infof("lines:%d users:%d\n", s_testline.line_num, s_testline.user_num);
-	
-	for (i = 0; i < s_testline.line_num; i++)
-	{
-		s_testline.lines[i] = i;
-	}
-	
-	for (i = 0; i < s_testline.user_num; i++)
-	{
-		s_testline.usrs[i].num = 0;
-	}
-
-	return 0;
+    va_list ap;
+    
+    if (level)
+    {
+        va_start(ap, fmt);
+        vprintf(fmt, ap);
+        va_end(ap);
+    }
 }
 
-static int get_oneuser_list(int user, char *str, int len)
+static int getUsersAndLines(char *str)
 {
-	int i = 0;
-	int j = 0;
-	int tmp = 0;
-	
-	for (i = 0; i < len; i++)
-	{
-		if (str[i] == ',')
-		{
-			s_testline.usrs[user].list[j] = tmp - 1;
-			j++;
+    int i = 0;
+    int ret = 0;
 
-			tmp = 0;
-		}
-		
-		if ((str[i] >= '0') && (str[i] <= '9'))
-		{
-			tmp *= 10;
-			tmp += (str[i] - '0');
-		}
-	}
+    ret = sscanf(str, "%d, %d", &s_testline.line_num, &s_testline.user_num);
+    if (ret < 0)
+    {
+        return -1;
+    }
 
-	s_testline.usrs[user].list[j] = tmp - 1;
-	j++;
-	
-	s_testline.usrs[user].num = j;
+    for (i = 0; i < s_testline.line_num; i++)
+    {
+        s_testline.lines[i] = i;
+    }
+    
+    for (i = 0; i < s_testline.user_num; i++)
+    {
+        s_testline.usrs[i].num = 0;
+    }
 
-	return 0;
+    return 0;
 }
 
-static int prase_file(char *path)
+static int getOneuserList(int user, char *str, int len)
 {
-	int i = 0;
-	int len = 0;
-	FILE *file = NULL;
-	char buff[512];
+    int i = 0;
+    int j = 0;
+    int tmp = 0;
+    
+    for (i = 0; i < len; i++)
+    {
+        if (str[i] == ',')
+        {
+            s_testline.usrs[user].list[j] = tmp - 1;
+            j++;
 
-	file = fopen(path , "r");
-	if (file == NULL)
-	{
-		errorf("fopen err\n");
-		return -1;
-	}
+            tmp = 0;
+        }
+        
+        if ((str[i] >= '0') && (str[i] <= '9'))
+        {
+            tmp *= TEN;
+            tmp += (str[i] - '0');
+        }
+    }
 
-	//users_and_lines
-	if (NULL != fgets(buff, sizeof(buff), file))
-	{
-		get_users_and_lines(buff);
-	}
+    s_testline.usrs[user].list[j] = tmp - 1;
+    j++;
+    
+    s_testline.usrs[user].num = j;
 
-	while (NULL != fgets(buff, sizeof(buff), file))
-	{
-		len = strnlen(buff, sizeof(buff));
-		get_oneuser_list(i, buff, len);
-		i++;
-	}
+    return 0;
+}
 
-	fclose(file);
+static int praseFile(char *path)
+{
+    int i = 0;
+    FILE *file = NULL;
+    char buff[512];
 
-	return 0;
+    file = fopen(path , "r");
+    if (file == NULL)
+    {
+        return -1;
+    }
+
+    //users_and_lines
+    if (NULL != fgets(buff, sizeof(buff), file))
+    {
+        getUsersAndLines(buff);
+    }
+
+    while (NULL != fgets(buff, sizeof(buff), file))
+    {
+        getOneuserList(i, buff, strnlen(buff, sizeof(buff)));
+        i++;
+    }
+
+    fclose(file);
+
+    return 0;
 }
 
 
-static int do_extract(int user, int *map)
+static int doExtract(int user, int *map)
 {
-	int i = 0;
-	int tmp = 0;
-	int sum = 0;
+    int i = 0;
+    int tmp = 0;
+    int sum = 0;
 
-	if (user == s_testline.user_num)
-	{
-		debugf("fuck\n");
-		return 1;
-	}
-	
-	for (i = 0; i < s_testline.usrs[user].num; i++)
-	{
-		tmp = s_testline.usrs[user].list[i];
-		if (map[tmp] == 0)
-		{
-			map[tmp] = 1;
-			sum += do_extract(user + 1, map);
-			map[tmp] = 0;
-		}
-
-	}
-	
-	return sum;
+    if (user == s_testline.user_num)
+    {
+        return 1;
+    }
+    
+    for (i = 0; i < s_testline.usrs[user].num; i++)
+    {
+        tmp = s_testline.usrs[user].list[i];
+        if (map[tmp] == 0)
+        {
+            map[tmp] = 1;
+            sum += doExtract(user + 1, map);
+            map[tmp] = 0;
+        }
+    }
+    
+    return sum;
 }
 
-static void do_process()
+static void doProcess()
 {
-	int tmp = 0;
+    int map[MAXLINE] = {0};
 
-	int map[MAX_LINE] = {0};
-
-	tmp = do_extract(0, map);
-	printf("%d\n", tmp);
+    myprintf(ONE, "%d\n", doExtract(0, map));
 }
 
 int main(int argc, char **argv)
 {
-	if ((argc != 2) || (argv[1] == NULL))
-	{
-		return -1;
-	}
+    if ((argc != TWO) || (argv[1] == NULL))
+    {
+        return -1;
+    }
 
-	prase_file(argv[1]);
+    praseFile(argv[1]);
 
-	do_process();
+    doProcess();
 
-	return 0;
+    return 0;
 }
