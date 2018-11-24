80a3385d4fba70714e1095fdb3648a4515adb019
sc <peterchenshen@gmail.com>
2018-11-24 22:37:56 +0800

    Update test4.cpp

--- test4.cpp
+++ test4.cpp
@@ -2,12 +2,18 @@
 #pragma GCC optimize ("-O3")
 #include <fstream>
 #include <iostream>
+#include <vector>
+#include <queue>
 #include <cstring>
 #include <string>
 #include <stdarg.h>
 #include <stdlib.h>
 
-#define MAXITEM    (1000)
+using namespace std;
+typedef long long LL;
+
+#define MAXITEM     (1000+16)
+#define MAXCAP      (0x3f3f3f3f3f3f3f3f)
 
 typedef enum
 {
@@ -26,22 +32,22 @@ typedef enum
 
 typedef struct
 {
-    int num;
-    int depend[MAXITEM];
-
-    int total;
+    LL num;
+    LL depend[MAXITEM];
 } item_t;
 
 typedef struct
 {
-    int item_num;
+    LL item_num;
     item_t items[MAXITEM];
+
+    LL total;
 
 } safe_t;
 
 static safe_t s_safe = {0};
 
-static void myprintf(int level, const char *fmt, ...)
+static void myprintf(LL level, const char *fmt, ...)
 {
     va_list ap;
     
@@ -53,11 +59,11 @@ static void myprintf(int level, const char *fmt, ...)
     }
 }
 
-static int getItemNum(char *str)
+static LL getItemNum(char *str)
 {
-    int ret = 0;
+    LL ret = 0;
 
-    ret = sscanf(str, "%d", &s_safe.item_num);
+    ret = sscanf(str, "%lld", &s_safe.item_num);
     if (ret < 0)
     {
         return -1;
@@ -66,12 +72,12 @@ static int getItemNum(char *str)
     return 0;
 }
 
-static int getOneitemList(int item, char *str, int len)
+static LL getOneitemList(LL item, char *str, LL len)
 {
-    int i = 0;
-    int j = 0;
-    int tmp = 0;
-    int flag = 0;
+    LL i = 0;
+    LL j = 0;
+    LL tmp = 0;
+    LL flag = 0;
     
     for (i = 0; i < len; i++)
     {
@@ -106,9 +112,9 @@ static int getOneitemList(int item, char *str, int len)
     return 0;
 }
 
-static int praseFile(char *path)
+static LL praseFile(char *path)
 {
-    int i = 0;
+    LL i = 0;
     FILE *file = NULL;
     char buff[512];
 
@@ -134,64 +140,157 @@ static int praseFile(char *path)
 
     return 0;
 }
-
-
-static int doSelect(int item, int *map, int *len)
+ 
+// 用于表示边的结构体（终点、容量、反向边）
+class edge
 {
-    int i = 0;
-    int tmp = 0;
-    
-    *len = *len + 1;
-    
-    if (map[item] == 1)
+public:
+    LL to;
+    LL cap;
+    LL rev;
+    edge(LL to, LL cap, LL rev) :to(to), cap(cap), rev(rev){}
+};
+
+ 
+static vector<edge> G[MAXITEM];  // 图的邻接表表示
+static LL level[MAXITEM];       // 顶点到源点的距离标号
+static LL iter[MAXITEM];        // 当前弧，在其之前的边已经没有用了
+ 
+// 向图中加入一条从from到to的容量为cap的边
+static void addEdge(LL from, LL to, LL cap)
+{
+    G[from].push_back(edge(to, cap, G[to].size() ));
+    G[to].push_back(edge(from, 0, G[from].size() - 1));
+}
+ 
+// 通过BFS计算从源点出发的距离标号
+static void bfs(LL s)
+{
+    LL i;
+
+    memset(level, -1, sizeof(level));
+    queue<LL> que;
+    level[s] = 0;
+    que.push(s);
+    while (!que.empty())
     {
-        return 0;
+        LL v = que.front(); 
+        que.pop();
+        for (i = 0; i < static_cast<LL>(G[v].size()); ++i)
+        {
+            edge& e = G[v][i];
+            if (e.cap > 0 && level[e.to] < 0)
+            {
+                level[e.to] = level[v] + 1;
+                que.push(e.to);
+            }
+        }
+    }
+}
+ 
+// 通过DFS寻找增广路
+static LL dfs(LL v, LL t, LL f)
+{
+    if (v == t)
+    {
+        return f;
     }
 
-    if (s_safe.items[item].total)
+    for (LL& i = iter[v]; i < static_cast<LL>(G[v].size()); ++i)
     {
-        return s_safe.items[item].total;
+        edge& e = G[v][i];
+        if (e.cap > 0 && level[v] < level[e.to])
+        {
+            LL d = dfs(e.to, t, min(f, e.cap));
+            if (d > 0)
+            {
+                e.cap -= d;
+                G[e.to][e.rev].cap += d;
+                return d;
+            }
+        }
     }
-
-    for (i = 1; i < s_safe.items[item].num; i++)
+ 
+    return 0;
+}
+ 
+// 求解从s到t的最大流
+static LL maxFlow(LL s, LL t)
+{
+    LL flow = 0;
+    for (;;)
     {
-        map[item] = 1;
-        tmp += doSelect(s_safe.items[item].depend[i] - 1, map, len);
-        map[item] = 0;
+        bfs(s);
+        if (level[t] < 0) 
+        {
+            return flow;
+        }
+        memset(iter, 0, sizeof(iter));
+        LL f;
+        while ((f = dfs(s, t, MAXCAP)) > 0)
+        {
+            flow += f;
+        }
     }
-    
-    tmp += s_safe.items[item].depend[0];
+}
+ 
+static LL visited[MAXITEM];
+// 遍历残余网络
+static void solve(LL v)
+{
+    LL i;
 
-    map[item] = 1;
-    s_safe.items[item].total = tmp;
-
-    return tmp;
+    visited[v] = true;
+    for (i = 0; i < static_cast<LL>(G[v].size()); ++i) 
+    {
+        const edge &e = G[v][i];
+        if (e.cap > 0 && !visited[e.to]) 
+        {
+            solve(e.to);
+        }
+    }
 }
 
 
 static void doProcess()
 {
-    int i = 0;
-    int sum = 0;
-    int len[MAXITEM] = {0};
-    int map[MAXITEM] = {0};
+    LL i;
+    LL j;
+    const LL s = 0;
+    const LL t = s_safe.item_num + 1;
 
     for (i = 0; i < s_safe.item_num; i++)
     {
-        if (s_safe.items[i].depend[0] <= 0)
+        LL w = s_safe.items[i].depend[0];
+        if (w > 0)
         {
-            continue;
+            s_safe.total += w;
+            addEdge(s, i+1, w);
         }
 
-        sum += doSelect(i, map, &len[i]);
+        if (w < 0)
+        {
+            addEdge(i+1, t, -w);
+        }
     }
 
-    myprintf(ONE, "%d\n", sum);
-    for (i = 0; i < s_safe.item_num; i++)
+    for (i = 0; i < s_safe.item_num; ++i)
     {
-        if (map[i])
+        for (j = 1; j < s_safe.items[i].num; j++)
         {
-            myprintf(ONE, "%d\n", i+1);
+            addEdge(i+1, s_safe.items[i].depend[j], MAXCAP);
+        }
+    }
+    
+    LL maxProfit = s_safe.total - maxFlow(s, t);
+    solve(s);
+
+    myprintf(ONE, "%lld\n", maxProfit);
+    for (i = 1; i <= s_safe.item_num; i++)
+    {
+        if (visited[i])
+        {
+            myprintf(ONE, "%lld\n", i);
         }
     }
 }
