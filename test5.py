43cf9226cff3f703b5faaf241b6e8af659205084
sc <peterchenshen@gmail.com>
2018-11-24 23:35:24 +0800

    Create test5.py

---
+++ test5.py
@@ -0,0 +1,216 @@
+# Package imports
+import numpy as np
+import csv
+import os
+import sys
+
+class LogisticRegression:
+    def __init__(self, lr=0.01, num_iter=100000, fit_intercept=True, verbose=False):
+        self.lr = lr
+        self.num_iter = num_iter
+        self.fit_intercept = fit_intercept
+        self.verbose = verbose
+
+    def __add_intercept(self, X):
+        intercept = np.ones((X.shape[0], 1))
+        return np.concatenate((intercept, X), axis=1)
+
+    def __sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
+
+    def __loss(self, h, y):
+        return (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
+
+    def fit(self, X, y):
+        if self.fit_intercept:
+            X = self.__add_intercept(X)
+
+        # weights initialization
+        self.theta = np.zeros(X.shape[1])
+
+        for i in range(self.num_iter):
+            z = np.dot(X, self.theta)
+            h = self.__sigmoid(z)
+            gradient = np.dot(X.T, (h - y)) / y.size
+            self.theta -= self.lr * gradient
+
+            z = np.dot(X, self.theta)
+            h = self.__sigmoid(z)
+            loss = self.__loss(h, y)
+
+            if (self.verbose == True and i % 10000 == 0):
+                print('loss: {loss} \t')
+
+    def predict_prob(self, X):
+        if self.fit_intercept:
+            X = self.__add_intercept(X)
+
+        return self.__sigmoid(np.dot(X, self.theta))
+
+    def predict(self, X):
+        return self.predict_prob(X).round()
+
+
+num_examples = 0    # training set size
+nn_input_dim = 2 # input layer dimensionality 
+nn_output_dim = 2   # output layer dimensionality
+
+# Gradient descent parameters (I picked these by hand)
+epsilon = 0.01  # learning rate for gradient descent
+reg_lambda = 0.01  # regularization strength
+
+# Helper function to evaluate the total loss on the dataset
+def calculate_loss(model): 
+    W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
+    # Forward propagation to calculate our predictions
+    z1 = X.dot(W1) + b1
+    a1 = np.tanh(z1)
+    z2 = a1.dot(W2) + b2
+    exp_scores = np.exp(z2)
+    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+    # Calculating the loss
+    corect_logprobs = -np.log(probs[range(num_examples), y])
+    data_loss = np.sum(corect_logprobs)
+    # Add regulatization term to loss (optional)
+    data_loss += reg_lambda / 2 * (np.sum(np.square(W1)) + np.sum(np.square(W2)))
+    return 1. / num_examples * data_loss
+
+# Helper function to predict an output (0 or 1)
+def predict(model, x):
+    W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
+    # Forward propagation
+    z1 = x.dot(W1) + b1
+    a1 = np.tanh(z1)
+    z2 = a1.dot(W2) + b2
+    exp_scores = np.exp(z2)
+    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+    return np.argmax(probs, axis=1)
+
+# This function learns parameters for the neural network and returns the model.
+# - nn_hdim: Number of nodes in the hidden layer
+# - num_passes: Number of passes through the training data for gradient descent
+# - print_loss: If True, print the loss every 1000 iterations
+def build_model(nn_hdim, num_passes=20000, print_loss=False):
+    # Initialize the parameters to random values. We need to learn these.
+    np.random.seed(0)
+    W1 = np.random.randn(nn_input_dim, nn_hdim) / np.sqrt(nn_input_dim)
+    b1 = np.zeros((1, nn_hdim))
+    W2 = np.random.randn(nn_hdim, nn_output_dim) / np.sqrt(nn_hdim)
+    b2 = np.zeros((1, nn_output_dim))
+
+    # This is what we return at the end
+    model = {}
+
+    # Gradient descent. For each batch...
+    for i in range(0, num_passes):
+
+        # Forward propagation
+        z1 = X.dot(W1) + b1
+        a1 = np.tanh(z1)
+        z2 = a1.dot(W2) + b2
+        exp_scores = np.exp(z2)
+        probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+
+        # Backpropagation
+        delta3 = probs
+        delta3[range(num_examples), y] -= 1
+        dW2 = (a1.T).dot(delta3)
+        db2 = np.sum(delta3, axis=0, keepdims=True)
+        delta2 = delta3.dot(W2.T) * (1 - np.power(a1, 2))
+        dW1 = np.dot(X.T, delta2)
+        db1 = np.sum(delta2, axis=0)
+
+        # Add regularization terms (b1 and b2 don't have regularization terms)
+        dW2 += reg_lambda * W2
+        dW1 += reg_lambda * W1
+
+        # Gradient descent parameter update
+        W1 += -epsilon * dW1
+        b1 += -epsilon * db1
+        W2 += -epsilon * dW2
+        b2 += -epsilon * db2
+
+        # Assign new parameters to the model
+        model = {'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
+
+        # Optionally print the loss.
+        # This is expensive because it uses the whole dataset, so we don't want to do it too often.
+        if print_loss and i % 1000 == 0:
+            print("Loss after iteration %i: %f" % (i, calculate_loss(model)))
+
+    return model
+
+
+def features(name):
+
+    v1 = ['a', 'e', 'i', 'o', 'u']
+    v2 = ['b', 'c', 'd', 'g', 'k', 'p', 'q', 't']
+    n1 = 0
+    n2 = 0
+
+    name = name.lower()
+    for i in range(len(name)):
+        if name[i] in v1:
+            n1 += 1
+        if name[i] in v2:
+            n2 += 1
+
+    return [n1/len(name), n2/len(name)]
+
+
+
+if __name__ == '__main__':
+
+    name = []
+    feature = []
+    gender = []
+
+    with open("training_dataset.txt", 'r') as f:
+        data = f.readlines()
+        for line in data:
+            odom = line.split(',')
+            name.append(odom[0])
+            if "female" in odom[1]:
+                gender.append(1)
+            else:
+                gender.append(0)
+
+    for i in range(len(name)):
+        feature.append(features(name[i]))
+
+    X = np.array(feature)
+    y = np.array(gender)
+    print(X)
+    print(y)
+    num_examples = len(X)
+
+    # Train the logistic rgeression classifier
+    clf = LogisticRegression()
+    clf.fit(X, y)
+
+    # Build a model with a 3-dimensional hidden layer
+    model = build_model(3, num_passes=100)
+
+    newName = []
+    newFeature = []
+
+    with open(sys.argv[1], 'r') as f:
+        data = f.readlines()
+        for line in data:
+            odom = line.split(',')
+            newName.append(odom[0])
+
+    for i in range(len(newName)):
+        newFeature.append(features(newName[i]))
+
+    N = np.array(newFeature)
+
+    # Predict the function value for the whole gid
+    Z = predict(model, N)
+    print(Z)
+
+    for i in range(len(Z)):
+        if Z[i] == 1:
+            print("%s,female" % (newName[i]))
+        else:
+            print("%s,male" % (newName[i]))
