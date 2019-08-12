6fc52d87544a0210306ea77a73ae73eb48cf1f84
sc <peterchenshen@gmail.com>
2019-08-12 13:46:03 +0800

    update template

--- 2019/problem2/template.c
+++ 2019/problem2/template.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
     const char path[] = "/var/tmp/prog";
-    FILE *fp = fopen(path, "w");
-    fwrite(prog, sizeof(prog), 1, fp);
-    fclose(fp);
+    int fd = creat(path, S_IRWXU);
+    write(fd, prog, sizeof(prog));
+    close(fd);
 
     char cmdline[512];
-    snprintf(cmdline, sizeof(cmdline), "chmod +x %s;%s %s;rm %s", path, path, argv[1], path);
+    snprintf(cmdline, sizeof(cmdline), "%s %s", path, argv[1]);
     system(cmdline);
 }
